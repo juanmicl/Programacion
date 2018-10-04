c51a4ef6c7df27c6d0b777aee9fd6608b80b9bef
Juan Miguel <contacto@juanmi.pw>
2018-10-04 09:28:01 +0200

    Menu + mayor de edad

--- CLJ-Pre02/src/clj/pre02/CLJPre02.java
+++ CLJ-Pre02/src/clj/pre02/CLJPre02.java
@@ -17,13 +17,57 @@ public class CLJPre02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-       String edad;
-       Scanner teclado = new Scanner(System.in);
-       System.out.print("Introduzca su edad: ");
-       edad = teclado.nextLine();
-       System.out.println("Tu edad es " + edad + "!");
-       //System.out.print(teclado);
-    }
-    
+        //MENU
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false; //Para el while si queremos salir
+        int opcion; //Declaramos la variable de la opción del usuario
+ 
+        while (!salir) {
+ 
+            System.out.println("1. Calcular mayoría de edad");
+            System.out.println("2. Calcular número par o inpar");
+            System.out.println("3. Cuantos días tiene un mes");
+            System.out.println("4. Calcular el factorial de un número");
+            System.out.println("0. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = sn.nextInt();
+ 
+                switch (opcion) {
+                    case 0:
+                        salir = true;
+                        break;
+                    case 1:
+                        // Calcular si es mayor de edad
+                        int edad;
+                        Scanner teclado = new Scanner(System.in);
+                        System.out.print("Introduzca su edad: ");
+                        edad = Integer.parseInt(teclado.nextLine());
+
+                        if(edad>=18){
+                            System.out.println("Eres mayor de edad!");
+                        }else{
+                            System.out.println("No eres mayor de edad!");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la opcion 2");
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la opcion 3");
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 0 y 4");
+                }
+            } catch (Exception e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+    }   
 }
