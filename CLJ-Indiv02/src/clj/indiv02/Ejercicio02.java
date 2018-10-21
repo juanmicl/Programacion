196c6a727fa0975e284fb83d7277aeb254982ea9
Juan Miguel <contacto@juanmi.pw>
2018-10-21 14:42:40 +0200

    añadimos ej

--- CLJ-Indiv02/src/clj/indiv02/Ejercicio02.java
+++ CLJ-Indiv02/src/clj/indiv02/Ejercicio02.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
  *  https://github.com/juanmicl/Programacion/tree/master/CLJ-Indiv02
  * @author Juanmi
  */
-public class Ejercicio01 {
-    // Identificar número positivo o negativo
+public class Ejercicio02 {
+    // Identificar número positivo o negativo y el cero decir que es cero
     public static void main(String[] args) {
         // Declaramos variables
         int numero;
@@ -19,10 +19,12 @@ public class Ejercicio01 {
             System.out.println("Introduce un número: ");
             numero = sc.nextInt();
             
-            if(numero >= 0){
+            if(numero > 0){
                 System.out.println("El número "+numero+" es POSITIVO");
-            }else{
+            }else if(numero < 0){
                 System.out.println("El número "+numero+" es NEGATIVO");
+            }else{
+                System.out.println("el número es cero");
             }
         }catch(InputMismatchException e){
             System.out.println("Debes introducir SOLO números!");
--- CLJ-Indiv02/src/clj/indiv02/Ejercicio03.java
+++ CLJ-Indiv02/src/clj/indiv02/Ejercicio03.java
@@ -8,21 +8,28 @@ import java.util.Scanner;
  *  https://github.com/juanmicl/Programacion/tree/master/CLJ-Indiv02
  * @author Juanmi
  */
-public class Ejercicio01 {
+public class Ejercicio03 {
     // Identificar número positivo o negativo
     public static void main(String[] args) {
         // Declaramos variables
-        int numero;
+        int numero1;
+        int numero2;
         Scanner sc = new Scanner(System.in);
         
         try{
+            // obtenemos numero1
             System.out.println("Introduce un número: ");
-            numero = sc.nextInt();
+            numero1 = sc.nextInt();
+            // obtenemos numero2
+            System.out.println("Introduce otro número: ");
+            numero2 = sc.nextInt();
             
-            if(numero >= 0){
-                System.out.println("El número "+numero+" es POSITIVO");
+            if(numero1 > numero2){
+                System.out.println("El número "+numero1+" es mayor que "+numero2);
+            }else if(numero1 < numero2){
+                System.out.println("El número "+numero1+" es menor que "+numero2);
             }else{
-                System.out.println("El número "+numero+" es NEGATIVO");
+                System.out.println("Los números son iguales");
             }
         }catch(InputMismatchException e){
             System.out.println("Debes introducir SOLO números!");
