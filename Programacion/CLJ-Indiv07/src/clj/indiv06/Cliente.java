9e072570ea11aa6d2a0d921b9a48038518f39b72
juanmi.pw <contacto@juanmi.pw>
2019-03-12 03:14:40 +0100

    Tarea online 7

---
+++ Programacion/CLJ-Indiv07/src/clj/indiv06/Cliente.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clj.indiv06;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author juanmi
+ */
+public class Cliente {
+    
+    private String dni;
+    private String nombre;
+    private String direccion;
+    private String localidad;
+    private String codigoPostal;
+
+    public Cliente(String dni, String nombre, String direccion, String localidad, String codigoPostal) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.localidad = localidad;
+        this.codigoPostal = codigoPostal;
+    }
+    
+    // Constructor copia
+    public Cliente(Cliente cliente) {
+        dni = cliente.dni;
+        nombre = cliente.nombre;
+        direccion = cliente.direccion;
+        localidad = cliente.localidad;
+        codigoPostal = cliente.codigoPostal;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "dni=" + dni + ", nombre=" + nombre + ", direccion=" + direccion + ", localidad=" + localidad + ", codigoPostal=" + codigoPostal + '}';
+    }
+}
---
+++ Programacion/CLJ-Indiv07/src/clj/indiv06/Mercancias.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clj.indiv06;
+
+/**
+ *
+ * @author 0101001011
+ */
+public abstract class Mercancias extends Vehiculo {
+    
+    private int pma;
+    private int volumen;
+
+    public Mercancias(int pma, int volumen, String matricula, String marca, String modelo, int cilindrada) {
+        super(matricula, marca, modelo, cilindrada);
+        this.pma = pma;
+        this.volumen = volumen;
+    }
+
+    public Mercancias(int pma, int volumen, Vehiculo vehiculo) {
+        super(vehiculo);
+        this.pma = pma;
+        this.volumen = volumen;
+    }
+
+    public int getPma() {
+        return pma;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    @Override
+    public String toString() {
+        return "pma:" + pma + ", volumen:" + volumen +", matricula: "+getMatricula()+", marca: "+getMarca()+", modelo: "+getModelo()+", cilindrada: "+getCilindrada();
+    }    
+}
