7795773f9e85e23d26986fce599e786160dd2bc9
juanmi.pw <contacto@juanmi.pw>
2019-04-29 18:18:47 +0200

    tarea online 9

---
+++ Programacion/CLJ-Indiv09/src/clj/indiv09/Utilidades.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clj.indiv09;
+
+import java.util.Scanner;
+/**
+ *
+ * @author juanmi
+ */
+public class Utilidades {
+    Scanner sc = new Scanner(System.in);
+    
+    // Constructor vacío porque no le vamos a pasar nada
+    public Utilidades() {
+    }
+    
+    /*
+    public Boolean comprobarMatricula(String matricula) {
+        matricula = matricula.toUpperCase();
+        return matricula.matches("^[0-9]{4}[B-DF-HJ-NP-TV-Z]{3}$") || matricula.matches("^[A-Z]{2}[0-9]{4}[A-Z]{2}$");
+    }
+    
+    public Boolean comprobarDni(String dni) {
+        dni = dni.toUpperCase();
+        return dni.matches("^[0-9]{8}[A-Z]$");
+    }
+    
+    public Boolean comprobarCodigoPostal(String cp) {
+        return cp.matches("^[01-52]{2}[0-9]{3}$");
+    }
+    
+    public Boolean comprobarTelefonoEsp(String telefono) {
+        return telefono.matches("^34[6789][0-9]{8}$");
+    } */
+    
+    public String comprobarMatricula(String matricula) {
+        matricula = matricula.toUpperCase();
+        while (!matricula.matches("^[0-9]{4}[B-DF-HJ-NP-TV-Z]{3}$") && !matricula.matches("^[A-Z]{2}[0-9]{4}[A-Z]{2}$")) {
+            System.out.println("Formato Matricula incorrecto.\n"
+                    + "Insertar Matricula: ");
+            matricula = sc.next();
+        }
+        return matricula;
+    }
+    
+    public String comprobarDni(String dni) {
+        dni = dni.toUpperCase();
+        while (!dni.matches("^[0-9]{8}[A-Z]$")) {
+            System.out.println("Formato DNI incorrecto.\n"
+                    + "Insertar DNI: ");
+            dni = sc.next();
+        }
+        return dni;
+    }
+    
+    public String comprobarCodigoPostal(String cp) {
+        while (!cp.matches("^[01-52]{2}[0-9]{3}$")) {
+            System.out.println("Formato Código Postal incorrecto.\n"
+                    + "Insertar Código Postal: ");
+            cp = sc.next();
+        }
+        return cp;
+    }
+    
+    public String comprobarTelefonoEsp(String telefono) {
+        while (!telefono.matches("^34[6789][0-9]{8}$")) {
+            System.out.println("Formato Teléfono incorrecto.\n"
+                    + "Insertar Teléfono: ");
+            telefono = sc.next();
+        }
+        return telefono;
+    }
+}
---
+++ Programacion/CLJ-Indiv09/src/clj/indiv09/Vehiculo.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clj.indiv09;
+
+/**
+ *
+ * @author juanmi
+ */
+public abstract class Vehiculo {
+    
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private int cilindrada;
+    private boolean disponible;
+
+    public Vehiculo(String matricula, String marca, String modelo, int cilindrada) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cilindrada = cilindrada;
+        this.disponible = true;
+    }
+    
+    // Constructor copia
+    public Vehiculo(Vehiculo vehiculo){
+        matricula = vehiculo.matricula;
+        marca = vehiculo.marca;
+        modelo = vehiculo.modelo;
+        cilindrada = vehiculo.cilindrada;
+        disponible = vehiculo.disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "matricula=" + matricula + ", marca=" + marca + ", modelo=" + modelo + ", cilindrada=" + cilindrada + ", disponible=" + disponible + '}';
+    }
+    
+}
