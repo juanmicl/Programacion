8901377406a7610e0bee694bb6d6363cd83aaaa4
juanmi.pw <contacto@juanmi.pw>
2019-04-29 22:53:26 +0200

    Avances

--- Programacion/CLJ-Indiv09/src/clj/indiv09/FXMLDocumentController.java
+++ Programacion/CLJ-Indiv09/src/clj/indiv09/FXMLDocumentController.java
@@ -5,31 +5,159 @@
  */
 package clj.indiv09;
 
+import java.awt.event.MouseEvent;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
 
 /**
  *
  * @author 0101001011
  */
 public class FXMLDocumentController implements Initializable {
+
+    private static ES ES = new ES();
+    private static Utilidades Utilidades = new Utilidades();
     
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    /**
+     * Nos devuelve si existe un cliente con dicho dni o null en caso contrario
+     * @param dni
+     * @return 
+     */
+    private Cliente getCliente(String dni) {
+        for (Cliente cliente : listClientesData) {
+            if (cliente.getDni().equalsIgnoreCase(dni)) {
+                return cliente;
+            }
+        }
+        return null;
     }
-    
+
+    @FXML
+    // Lista Clientes
+    private ListView<Cliente> listClientes;
+    private ObservableList<Cliente> listClientesData = FXCollections.observableArrayList();
+
+    // TextFields Clientes
+    @FXML
+    private TextField txtClientesNif = new TextField();
+    @FXML
+    private TextField txtClientesNombre = new TextField();
+    @FXML
+    private TextField txtClientesDireccion = new TextField();
+    @FXML
+    private TextField txtClientesLocalidad = new TextField();
+    @FXML
+    private TextField txtClientesCP = new TextField();
+
+    @FXML
+    public void loadClientes() {
+        if (listClientes.getSelectionModel().getSelectedItem() != null) {
+            System.out.println(listClientes.getSelectionModel().getSelectedItem().getNombre());
+            txtClientesNif.setText(listClientes.getSelectionModel().getSelectedItem().getDni());
+            txtClientesNombre.setText(listClientes.getSelectionModel().getSelectedItem().getNombre());
+            txtClientesDireccion.setText(listClientes.getSelectionModel().getSelectedItem().getDireccion());
+            txtClientesLocalidad.setText(listClientes.getSelectionModel().getSelectedItem().getLocalidad());
+            txtClientesCP.setText(listClientes.getSelectionModel().getSelectedItem().getCodigoPostal());
+        }
+    }
+
+    @FXML
+    public void anadirCliente() {
+        Cliente cliente;
+        cliente = getCliente(txtClientesNif.getText());
+        System.out.println(cliente);
+        if (cliente == null) {
+            if (Utilidades.comprobarDni(txtClientesNif.getText()) != null && Utilidades.comprobarCodigoPostal(txtClientesCP.getText()) != null) {
+                // Introducimos cliente
+                listClientesData.add(new Cliente(
+                    txtClientesNif.getText().toUpperCase(),
+                    txtClientesNombre.getText(),
+                    txtClientesDireccion.getText(),
+                    txtClientesLocalidad.getText(),
+                    txtClientesCP.getText()
+                ));
+            } else {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Error al añadir objeto");
+                alert.setContentText("Codigo postal o DNI incorrectos!");
+                alert.showAndWait();
+            }
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Error al añadir objeto");
+                alert.setContentText("Este cliente ya se encuentra en la base de datos!");
+                alert.showAndWait();
+        }
+        
+    }
+
+    @FXML
+    public void borrarCliente() {
+        if (listClientes.getSelectionModel().getSelectedItem() != null) {
+            System.out.println("Borrando -> " + listClientes.getSelectionModel().getSelectedItem().getNombre());
+            Cliente tempCliente = listClientes.getSelectionModel().getSelectedItem();
+
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmación");
+            alert.setHeaderText("Necesitamos su confirmación");
+            alert.setContentText("¿Está seguro de eliminar a " + listClientes.getSelectionModel().getSelectedItem().getNombre() + "?");
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.get() == ButtonType.OK) {
+                // borramos cliente
+                listClientes.getSelectionModel().select(-1);
+                listClientesData.remove(tempCliente);
+            } else {
+                // no ocurre nada
+            }
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error al eliminar objeto");
+            alert.setContentText("Necesitas tener un objeto seleccionado para poder eliminarlo!");
+
+            alert.showAndWait();
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        // metemos datos de prueba
+        listClientesData.add(new Cliente("11232311A", "Pepe Lopez", "Calle de arriba", "Vicar", "04720"));
+        listClientesData.add(new Cliente("11232999B", "Andrea Perez", "Calle de en medio", "Vicar", "04720"));
+        listClientesData.add(new Cliente("11456711C", "Alex Romero", "Calle de abajo", "Vicar", "04720"));
+
+        listClientes.setItems(listClientesData);
+
+        listClientes.setCellFactory(new Callback<ListView<Cliente>, ListCell<Cliente>>() {
+            @Override
+            public ListCell<Cliente> call(ListView<Cliente> param) {
+                ListCell<Cliente> cell = new ListCell<Cliente>() {
+                    @Override
+                    protected void updateItem(Cliente cli, boolean bln) {
+                        super.updateItem(cli, bln);
+                        if (cli == null) {
+                            setGraphic(null);
+                        } else {
+                            Text t = new Text(cli.getDni() + "  -  " + cli.getNombre());
+                            setGraphic(t);
+                        }
+                    }
+                };
+                return cell;
+            }
+        });
+    }
 }
--- Programacion/CLJ-Indiv09/src/clj/indiv09/Utilidades.java
+++ Programacion/CLJ-Indiv09/src/clj/indiv09/Utilidades.java
@@ -6,17 +6,19 @@
 package clj.indiv09;
 
 import java.util.Scanner;
+
 /**
  *
  * @author juanmi
  */
 public class Utilidades {
+
     Scanner sc = new Scanner(System.in);
-    
+
     // Constructor vacío porque no le vamos a pasar nada
     public Utilidades() {
     }
-    
+
     /*
     public Boolean comprobarMatricula(String matricula) {
         matricula = matricula.toUpperCase();
@@ -35,42 +37,39 @@ public class Utilidades {
     public Boolean comprobarTelefonoEsp(String telefono) {
         return telefono.matches("^34[6789][0-9]{8}$");
     } */
-    
     public String comprobarMatricula(String matricula) {
         matricula = matricula.toUpperCase();
-        while (!matricula.matches("^[0-9]{4}[B-DF-HJ-NP-TV-Z]{3}$") && !matricula.matches("^[A-Z]{2}[0-9]{4}[A-Z]{2}$")) {
-            System.out.println("Formato Matricula incorrecto.\n"
-                    + "Insertar Matricula: ");
-            matricula = sc.next();
+        if (!matricula.matches("^[0-9]{4}[B-DF-HJ-NP-TV-Z]{3}$") && !matricula.matches("^[A-Z]{2}[0-9]{4}[A-Z]{2}$")) {
+            return null;
+        } else {
+            return matricula;
         }
-        return matricula;
     }
-    
+
     public String comprobarDni(String dni) {
         dni = dni.toUpperCase();
-        while (!dni.matches("^[0-9]{8}[A-Z]$")) {
-            System.out.println("Formato DNI incorrecto.\n"
-                    + "Insertar DNI: ");
-            dni = sc.next();
+        if (!dni.matches("^[0-9]{8}[A-Z]$")) {
+            return null;
+        } else {
+            return dni;
         }
-        return dni;
     }
-    
+
     public String comprobarCodigoPostal(String cp) {
-        while (!cp.matches("^[01-52]{2}[0-9]{3}$")) {
-            System.out.println("Formato Código Postal incorrecto.\n"
-                    + "Insertar Código Postal: ");
-            cp = sc.next();
+        if (!cp.matches("^[01-52]{2}[0-9]{3}$")) {
+            return null;
+        } else {
+            return cp;
         }
-        return cp;
     }
-    
+
     public String comprobarTelefonoEsp(String telefono) {
-        while (!telefono.matches("^34[6789][0-9]{8}$")) {
-            System.out.println("Formato Teléfono incorrecto.\n"
-                    + "Insertar Teléfono: ");
-            telefono = sc.next();
+        if (!telefono.matches("^34[6789][0-9]{8}$")) {
+            return null;
+        } else {
+            return telefono;
         }
-        return telefono;
+
     }
 }
+
