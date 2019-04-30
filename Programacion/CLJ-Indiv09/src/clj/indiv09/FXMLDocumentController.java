3de88d314fe543293e7d65d7c0ed36fba64bfb1c
juanmi.pw <contacto@juanmi.pw>
2019-04-30 22:10:34 +0200

    mas cosas

--- Programacion/CLJ-Indiv09/src/clj/indiv09/FXMLDocumentController.java
+++ Programacion/CLJ-Indiv09/src/clj/indiv09/FXMLDocumentController.java
@@ -28,15 +28,28 @@ public class FXMLDocumentController implements Initializable {
     private static ES ES = new ES();
     private static Utilidades Utilidades = new Utilidades();
     
-    /**
-     * Nos devuelve si existe un cliente con dicho dni o null en caso contrario
-     * @param dni
-     * @return 
-     */
     private Cliente getCliente(String dni) {
         for (Cliente cliente : listClientesData) {
             if (cliente.getDni().equalsIgnoreCase(dni)) {
                 return cliente;
+            }
+        }
+        return null;
+    }
+    
+    private Vehiculo getVehiculo(String matricula) {
+        for (Vehiculo vehiculo : listVehiculosData) {
+            if (vehiculo.getMatricula().equalsIgnoreCase(matricula)) {
+                return vehiculo;
+            }
+        }
+        return null;
+    }
+    
+    private Alquiler getAlquiler(Cliente cliente, Vehiculo vehiculo) {
+        for (Alquiler alquiler : listAlquileresData) {
+            if (alquiler.getCliente() == cliente && alquiler.getVehiculo() == vehiculo) {
+                return alquiler;
             }
         }
         return null;
@@ -58,6 +71,207 @@ public class FXMLDocumentController implements Initializable {
     private TextField txtClientesLocalidad = new TextField();
     @FXML
     private TextField txtClientesCP = new TextField();
+    
+    @FXML
+    // Lista Vehículos
+    private ListView<Vehiculo> listVehiculos;
+    private ObservableList<Vehiculo> listVehiculosData = FXCollections.observableArrayList();
+    
+    // TextFields Vehiculos
+    @FXML
+    private TextField txtVehMatricula = new TextField();
+    @FXML
+    private TextField txtVehMarca = new TextField();
+    @FXML
+    private TextField txtVehModelo = new TextField();
+    @FXML
+    private TextField txtVehCilindrada = new TextField();
+    @FXML
+    private CheckBox chkVehDisponible = new CheckBox();
+    
+    @FXML
+    // Lista Alquileres
+    private ListView<Alquiler> listAlquileres;
+    private ObservableList<Alquiler> listAlquileresData = FXCollections.observableArrayList();
+
+    // TextFields Alquileres
+    @FXML
+    private TextField txtAlqNif = new TextField();
+    @FXML
+    private TextField txtAlqNombre = new TextField();
+    @FXML
+    private TextField txtAlqDireccion = new TextField();
+    @FXML
+    private TextField txtAlqLocalidad = new TextField();
+    @FXML
+    private TextField txtAlqCP = new TextField();
+    @FXML
+    private TextField txtAlqMatricula = new TextField();
+    @FXML
+    private TextField txtAlqMarca = new TextField();
+    @FXML
+    private TextField txtAlqModelo = new TextField();
+    @FXML
+    private TextField txtAlqCilindrada = new TextField();
+    @FXML
+    private CheckBox chkAlqDisponible = new CheckBox();
+    
+    
+    @FXML
+    public void loadAlquileres() {
+        if (listAlquileres.getSelectionModel().getSelectedItem() != null) {
+            txtAlqNif.setText(listAlquileres.getSelectionModel().getSelectedItem().getCliente().getDni());
+            txtAlqNombre.setText(listAlquileres.getSelectionModel().getSelectedItem().getCliente().getNombre());
+            txtAlqDireccion.setText(listAlquileres.getSelectionModel().getSelectedItem().getCliente().getDireccion());
+            txtAlqLocalidad.setText(String.valueOf(listAlquileres.getSelectionModel().getSelectedItem().getCliente().getLocalidad()));
+            txtAlqCP.setText(listAlquileres.getSelectionModel().getSelectedItem().getCliente().getCodigoPostal());
+            txtAlqMatricula.setText(listAlquileres.getSelectionModel().getSelectedItem().getVehiculo().getMatricula());
+            txtAlqMarca.setText(listAlquileres.getSelectionModel().getSelectedItem().getVehiculo().getMarca());
+            txtAlqModelo.setText(listAlquileres.getSelectionModel().getSelectedItem().getVehiculo().getModelo());
+            txtAlqCilindrada.setText(String.valueOf(listAlquileres.getSelectionModel().getSelectedItem().getVehiculo().getCilindrada()));
+            chkAlqDisponible.setSelected(listAlquileres.getSelectionModel().getSelectedItem().getVehiculo().isDisponible());
+        }
+    }
+    
+    @FXML
+    public void anadirAlquiler() {
+        Vehiculo vehiculo;
+        Cliente cliente;
+        Alquiler alquiler;
+        vehiculo = getVehiculo(txtAlqMatricula.getText());
+        cliente = getCliente(txtAlqNif.getText());
+        alquiler = getAlquiler(cliente, vehiculo);
+        if (vehiculo != null && cliente != null) {
+            if (alquiler == null) {
+                if (Utilidades.comprobarMatricula(txtAlqMatricula.getText()) != null && Utilidades.comprobarDni(txtAlqNif.getText()) != null) {
+                    // Introducimos vehiculo
+                    listAlquileresData.add(new Alquiler(cliente, vehiculo));
+                } else {
+                    Alert alert = new Alert(AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText("Error al añadir objeto");
+                    alert.setContentText("Matricula o DNI incorrecta!");
+                    alert.showAndWait();
+                }
+            } else {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Error al añadir objeto");
+                alert.setContentText("Este Alquiler ya se encuentra activo!");
+                alert.showAndWait();
+            }
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error al añadir objeto");
+            alert.setContentText("Este vehículo o DNI no se encuentran en la base de datos!");
+            alert.showAndWait();
+        }
+    }
+    
+    @FXML
+    public void borrarAlquiler() {
+        if (listAlquileres.getSelectionModel().getSelectedItem() != null) {
+            Alquiler tempAlquiler = listAlquileres.getSelectionModel().getSelectedItem();
+
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmación");
+            alert.setHeaderText("Necesitamos su confirmación");
+            alert.setContentText("¿Está seguro de eliminar este alquiler?");
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.get() == ButtonType.OK) {
+                // borramos vehiculo
+                listAlquileres.getSelectionModel().select(-1);
+                listAlquileresData.remove(tempAlquiler);
+            } else {
+                // no ocurre nada
+            }
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error al eliminar objeto");
+            alert.setContentText("Necesitas tener un objeto seleccionado para poder eliminarlo!");
+            alert.showAndWait();
+        }
+    }
+    
+    @FXML
+    public void loadVehiculos() {
+        if (listVehiculos.getSelectionModel().getSelectedItem() != null) {
+            System.out.println(listVehiculos.getSelectionModel().getSelectedItem().getMatricula());
+            txtVehMatricula.setText(listVehiculos.getSelectionModel().getSelectedItem().getMatricula());
+            txtVehMarca.setText(listVehiculos.getSelectionModel().getSelectedItem().getMarca());
+            txtVehModelo.setText(listVehiculos.getSelectionModel().getSelectedItem().getModelo());
+            txtVehCilindrada.setText(String.valueOf(listVehiculos.getSelectionModel().getSelectedItem().getCilindrada()));
+            chkVehDisponible.setSelected(listVehiculos.getSelectionModel().getSelectedItem().isDisponible());
+        }
+    }
+    
+    @FXML
+    public void anadirVehiculo() {
+        Vehiculo vehiculo;
+        vehiculo = getVehiculo(txtVehMatricula.getText());
+        System.out.println(vehiculo);
+        if (vehiculo == null) {
+            //ES.escribirLn(Utilidades.comprobarMatricula(txtVehMatricula.getText()));
+            if (Utilidades.comprobarMatricula(txtVehMatricula.getText()) != null) {
+                // Introducimos vehiculo
+                listVehiculosData.add(new Familiar(
+                    0,
+                    false,
+                    0,
+                    Enumerados.TipoCombustible.DIESEL,
+                    txtVehMatricula.getText().toUpperCase(),
+                    txtVehMarca.getText(),
+                    txtVehModelo.getText(),
+                    Integer.parseInt(txtVehCilindrada.getText())
+                ));
+            } else {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Error al añadir objeto");
+                alert.setContentText("Matricula incorrecta!");
+                alert.showAndWait();
+            }
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Error al añadir objeto");
+                alert.setContentText("Este vehículo ya se encuentra en la base de datos!");
+                alert.showAndWait();
+        }
+        
+    }
+    
+    @FXML
+    public void borrarVehiculo() {
+        if (listVehiculos.getSelectionModel().getSelectedItem() != null) {
+            System.out.println("Borrando -> " + listVehiculos.getSelectionModel().getSelectedItem().getMatricula());
+            Vehiculo tempVehiculo = listVehiculos.getSelectionModel().getSelectedItem();
+
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setTitle("Confirmación");
+            alert.setHeaderText("Necesitamos su confirmación");
+            alert.setContentText("¿Está seguro de eliminar a " + listVehiculos.getSelectionModel().getSelectedItem().getMatricula() + "?");
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.get() == ButtonType.OK) {
+                // borramos vehiculo
+                listVehiculos.getSelectionModel().select(-1);
+                listVehiculosData.remove(tempVehiculo);
+            } else {
+                // no ocurre nada
+            }
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error al eliminar objeto");
+            alert.setContentText("Necesitas tener un objeto seleccionado para poder eliminarlo!");
+
+            alert.showAndWait();
+        }
+    }
 
     @FXML
     public void loadClientes() {
@@ -100,7 +314,6 @@ public class FXMLDocumentController implements Initializable {
                 alert.setContentText("Este cliente ya se encuentra en la base de datos!");
                 alert.showAndWait();
         }
-        
     }
 
     @FXML
@@ -131,6 +344,15 @@ public class FXMLDocumentController implements Initializable {
             alert.showAndWait();
         }
     }
+    
+    @FXML
+    public void acercade() {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("A cerca de");
+        alert.setHeaderText("Información a cerca del programa");
+        alert.setContentText("Creado por Juan Miguel Contreras.");
+        alert.showAndWait();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -138,8 +360,15 @@ public class FXMLDocumentController implements Initializable {
         listClientesData.add(new Cliente("11232311A", "Pepe Lopez", "Calle de arriba", "Vicar", "04720"));
         listClientesData.add(new Cliente("11232999B", "Andrea Perez", "Calle de en medio", "Vicar", "04720"));
         listClientesData.add(new Cliente("11456711C", "Alex Romero", "Calle de abajo", "Vicar", "04720"));
-
+        // metemos vehiculos de prueba
+        listVehiculosData.add(new Familiar(0, true, 0, Enumerados.TipoCombustible.DIESEL, "1111BAB", "Audi", "A3", 0));
+        listVehiculosData.add(new Familiar(0, true, 0, Enumerados.TipoCombustible.DIESEL, "2222BBB", "Citroen", "C3", 0));
+        // metemos alquileres de prueba
+        listAlquileresData.add(new Alquiler(listClientesData.get(0), listVehiculosData.get(0)));
+        
         listClientes.setItems(listClientesData);
+        listVehiculos.setItems(listVehiculosData);
+        listAlquileres.setItems(listAlquileresData);
 
         listClientes.setCellFactory(new Callback<ListView<Cliente>, ListCell<Cliente>>() {
             @Override
@@ -152,6 +381,44 @@ public class FXMLDocumentController implements Initializable {
                             setGraphic(null);
                         } else {
                             Text t = new Text(cli.getDni() + "  -  " + cli.getNombre());
+                            setGraphic(t);
+                        }
+                    }
+                };
+                return cell;
+            }
+        });
+        
+        listVehiculos.setCellFactory(new Callback<ListView<Vehiculo>, ListCell<Vehiculo>>() {
+            @Override
+            public ListCell<Vehiculo> call(ListView<Vehiculo> param) {
+                ListCell<Vehiculo> cell = new ListCell<Vehiculo>() {
+                    @Override
+                    protected void updateItem(Vehiculo veh, boolean bln) {
+                        super.updateItem(veh, bln);
+                        if (veh == null) {
+                            setGraphic(null);
+                        } else {
+                            Text t = new Text(veh.getMatricula() + "  -  " + veh.getMarca()+"  -  "+veh.getModelo());
+                            setGraphic(t);
+                        }
+                    }
+                };
+                return cell;
+            }
+        });
+        
+        listAlquileres.setCellFactory(new Callback<ListView<Alquiler>, ListCell<Alquiler>>() {
+            @Override
+            public ListCell<Alquiler> call(ListView<Alquiler> param) {
+                ListCell<Alquiler> cell = new ListCell<Alquiler>() {
+                    @Override
+                    protected void updateItem(Alquiler alq, boolean bln) {
+                        super.updateItem(alq, bln);
+                        if (alq == null) {
+                            setGraphic(null);
+                        } else {
+                            Text t = new Text(alq.getCliente().getDni() + "  -  " + alq.getVehiculo().getMatricula());
                             setGraphic(t);
                         }
                     }
